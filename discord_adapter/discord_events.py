beb074424bb8ed37c9fb30c6c77969e0a60b7bf9
minyinpop <minyinpop@gmail.com>
2026-06-22 22:24:19 +0800

    移除 pass

--- discord_adapter/discord_events.py
+++ discord_adapter/discord_events.py
@@ -1,30 +1,13 @@
 import discord
-from discord import DiscordException
 
 async def send_message(channel: discord.TextChannel, content: str):
-    try:
-        await channel.send(content)
-
-    except DiscordException:
-        pass
+    await channel.send(content)
 
 async def reply_message(message: discord.Message, content: str):
-    try:
-        await message.reply(content)
-
-    except DiscordException:
-        pass
+    await message.reply(content)
 
 async def add_reaction(message: discord.Message, emoji: str):
-    try:
-        await message.add_reaction(emoji)
-
-    except DiscordException:
-        pass
+    await message.add_reaction(emoji)
 
 async def remove_reaction(message: discord.Message, emoji: str, member: discord.Member):
-    try:
-        await message.remove_reaction(emoji, member)
-
-    except DiscordException:
-        pass
+    await message.remove_reaction(emoji, member)
