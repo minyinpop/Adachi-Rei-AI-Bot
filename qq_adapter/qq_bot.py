2b97f1e20aa5668f69eedfad82159142396d7dd7
minyinpop <minyinpop@gmail.com>
2026-06-26 01:08:22 +0800

    新增訊息回覆排程

--- qq_adapter/qq_bot.py
+++ qq_adapter/qq_bot.py
@@ -9,148 +9,163 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+MESSAGE_QUEUE = asyncio.Queue()
+
 async def handle_onebot(websocket):
     print("【✅】NapCat 連線成功")
 
+    asyncio.create_task(message_worker())
+
     try:
         async for message in websocket:
-            message = json.loads(message)
-
-            print("【💬】訊息傳入")
-            print(json.dumps(
-                message,
-                ensure_ascii=False,
-                indent=4
-            ))
-
-            match message["post_type"]:
-                case "message_sent":
-                    has_at = False
-
-                    sender_message = {
-                        "id": message["sender"]["user_id"],
-                        "name": message["sender"]["nickname"],
-                        "role": "user",
-                        "message": []
-                    }
-
-                    match message["message_type"]:
-                        case "group":
-                            if message["group_id"] != 689904439:
-                                continue
-
-                            for segment in message["message"]:
-                                match segment["type"]:
-                                    case "at":
-                                        if int(segment["data"]["qq"]) == message["self_id"]:
-                                            if has_at:
-                                                # Bot 已經被 @ 但又被 @，就把 Bot 的名字給加入到文字裡
-                                                sender_message["message"].append(message["sender"]["nickname"])
-
-                                            else:
-                                                # Bot 第一次被 @
-                                                has_at = True
-
-                                            continue
-
-                                        else:
-                                            # 其他人被 @，就去查詢那個人的名稱，然後添加到文字裡
-                                            response = requests.post(
-                                                url="http://127.0.0.1:3001/get_group_member_info",
-                                                headers={
-                                                    "Authorization": os.getenv("QQ_BOT_TOKEN")
-                                                },
-                                                json={
-                                                    "group_id": message["group_id"],
-                                                    "user_id": int(segment["data"]["qq"])
-                                                }
-                                            )
-
-                                            print("=== 獲取成員資料 ===")
-                                            print(json.dumps(
-                                                response.json(),
-                                                ensure_ascii=False,
-                                                indent=4
-                                            ))
-
-                                            sender_message["message"].append(response.json()["data"]["nickname"])
-
-                                    case "text":
-                                        sender_message["message"].append(segment["data"]["text"])
-
-                                    case _:
-                                        print(f"【❗】未被登記的 message：{segment['type']}")
-                                        continue
-
-                            if not has_at:
-                                continue
-
-                            sender_message["message"] = "".join(sender_message["message"])
-
-                            requests.post(
-                                url="http://127.0.0.1:3001/send_group_msg",
-                                headers={
-                                    "Authorization": os.getenv("QQ_BOT_TOKEN")
-                                },
-                                json={
-                                    "group_id": message["group_id"],
-                                    "message": [
-                                        {
-                                            "type": "text",
-                                            "data": {
-                                                "text": "正在思考中......"
-                                            }
-                                        }
-                                    ]
-                                }
-                            )
-
-                            ai_response = await ask_ollama(
-                                sender_message=sender_message,
-                                short_memory_messages=[],
-                                long_memory=[]
-                            )
-
-                            requests.post(
-                                url="http://127.0.0.1:3001/send_group_msg",
-                                headers={
-                                    "Authorization": os.getenv("QQ_BOT_TOKEN")
-                                },
-                                json={
-                                    "group_id": message["group_id"],
-                                    "message": [
-                                        {
-                                            "type": "reply",
-                                            "data": {
-                                                "id": str(message["message_id"])
-                                            }
-                                        },
-                                        {
-                                            "type": "text",
-                                            "data": {
-                                                "text": ai_response
-                                            }
-                                        }
-                                    ]
-                                }
-                            )
-
-                            continue
-
-                        case "private":
-                            print("【❗】私人回覆尚未實作")
-                            continue
-
-                        case _:
-                            print(f"【❗】未被登記的 message_type：{message['message_type']}")
-                            continue
-
-                case _:
-                    print(f"【❗】未被登記的 post_type：{message['post_type']}")
-                    continue
+            await MESSAGE_QUEUE.put(message)
 
     except websockets.ConnectionClosed:
         print("【🛑】NapCat 連線中斷")
+
+async def message_worker():
+    while True:
+        message = await MESSAGE_QUEUE.get()
+
+        await message_handler(message)
+
+        MESSAGE_QUEUE.task_done()
+
+async def message_handler(message):
+    message = json.loads(message)
+
+    print("【💬】訊息傳入")
+    print(json.dumps(
+        message,
+        ensure_ascii=False,
+        indent=4
+    ))
+
+    match message["post_type"]:
+        case "message_sent":
+            has_at = False
+
+            sender_message = {
+                "id": message["sender"]["user_id"],
+                "name": message["sender"]["nickname"],
+                "role": "user",
+                "message": []
+            }
+
+            match message["message_type"]:
+                case "group":
+                    if message["group_id"] != 689904439:
+                        return
+
+                    for segment in message["message"]:
+                        match segment["type"]:
+                            case "at":
+                                if int(segment["data"]["qq"]) == message["self_id"]:
+                                    if has_at:
+                                        # Bot 已經被 @ 但又被 @，就把 Bot 的名字給加入到文字裡
+                                        sender_message["message"].append(message["sender"]["nickname"])
+
+                                    else:
+                                        # Bot 第一次被 @
+                                        has_at = True
+
+                                    continue
+
+                                else:
+                                    # 其他人被 @，就去查詢那個人的名稱，然後添加到文字裡
+                                    response = requests.post(
+                                        url="http://127.0.0.1:3001/get_group_member_info",
+                                        headers={
+                                            "Authorization": os.getenv("QQ_BOT_TOKEN")
+                                        },
+                                        json={
+                                            "group_id": message["group_id"],
+                                            "user_id": int(segment["data"]["qq"])
+                                        }
+                                    )
+
+                                    print("=== 獲取成員資料 ===")
+                                    print(json.dumps(
+                                        response.json(),
+                                        ensure_ascii=False,
+                                        indent=4
+                                    ))
+
+                                    sender_message["message"].append(response.json()["data"]["nickname"])
+
+                            case "text":
+                                sender_message["message"].append(segment["data"]["text"])
+
+                            case _:
+                                print(f"【❗】未被登記的 message：{segment['type']}")
+                                continue
+
+                    if not has_at:
+                        return
+
+                    sender_message["message"] = "".join(sender_message["message"])
+
+                    requests.post(
+                        url="http://127.0.0.1:3001/send_group_msg",
+                        headers={
+                            "Authorization": os.getenv("QQ_BOT_TOKEN")
+                        },
+                        json={
+                            "group_id": message["group_id"],
+                            "message": [
+                                {
+                                    "type": "text",
+                                    "data": {
+                                        "text": "正在思考中......"
+                                    }
+                                }
+                            ]
+                        }
+                    )
+
+                    ai_response = await ask_ollama(
+                        sender_message=sender_message,
+                        short_memory_messages=[],
+                        long_memory=[]
+                    )
+
+                    requests.post(
+                        url="http://127.0.0.1:3001/send_group_msg",
+                        headers={
+                            "Authorization": os.getenv("QQ_BOT_TOKEN")
+                        },
+                        json={
+                            "group_id": message["group_id"],
+                            "message": [
+                                {
+                                    "type": "reply",
+                                    "data": {
+                                        "id": str(message["message_id"])
+                                    }
+                                },
+                                {
+                                    "type": "text",
+                                    "data": {
+                                        "text": ai_response
+                                    }
+                                }
+                            ]
+                        }
+                    )
+
+                    return
+
+                case "private":
+                    print("【❗】私人回覆尚未實作")
+                    return
+
+                case _:
+                    print(f"【❗】未被登記的 message_type：{message['message_type']}")
+                    return
+
+        case _:
+            print(f"【❗】未被登記的 post_type：{message['post_type']}")
+            return
 
 async def main():
     server = await websockets.serve(
